f2616b42a010ce08a72b0871f28665ac8be799e1
asmitha2478-ops <asmitha2478@gmail.com>
2026-04-03 20:49:13 +0530

    Update model.js

--- model.js
+++ model.js
@@ -1,13 +1,10 @@
-// model.js
-const mongoose = require("./connection"); // import the Mongoose module
+const mongoose = require("./connection"); 
 
-// Define the feedback schema
 const feedbackSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     message: { type: String, required: true, maxlength: 360 }
 }, { timestamps: true });
 
-// Create and export the model
 const Feedback = mongoose.model("Feedback", feedbackSchema);
 module.exports = Feedback;
